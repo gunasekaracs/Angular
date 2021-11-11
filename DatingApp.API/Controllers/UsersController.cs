8ab9cd21ef59417f77744af463f736373f1b2790
gunasekc <gunasekc@BDC-TDOAL-D0342>
2021-11-11 10:13:56 +0800

    end of section 10

--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -1,8 +1,10 @@
 ﻿using API.Dtos;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace API.Controllers
@@ -10,10 +12,12 @@ namespace API.Controllers
     [Authorize]
     public class UsersController : BaseController
     {
+        readonly IMapper mapper;
         readonly IUserRepository userRepository;
-        public UsersController(IUserRepository userRepository)
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
             this.userRepository = userRepository;
+            this.mapper = mapper;
         }
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
@@ -24,6 +28,16 @@ namespace API.Controllers
         public async Task<ActionResult<MemberDto>> GetUser(string userName)
         {
             return await userRepository.GetMemberAsync(userName);
+        }
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await userRepository.GetUserByUsernameAsync(username);
+            mapper.Map(memberUpdateDto, user);
+            userRepository.Update(user);
+            if (await userRepository.SaveAllAsync()) return NoContent();
+            return BadRequest("Fail to update user");
         }
     }
 }
